f4e2f6524635a0a445379f3352cf3b7de7440f17
Kehn <ezeokpokc@yahoo.com>
2025-05-02 14:17:20 +0100

    Action Card changes

--- src/app/card/page.tsx
+++ src/app/card/page.tsx
@@ -13,7 +13,7 @@ export default function CardPage() {
       <TopNavbar />
 
       <main className="flex-grow flex flex-col items-center justify-start px-4 py-10 text-center">
-        <h1 className="text-4xl font-bold text-blue-600 mb-6">Cards</h1>
+        <h1 className="text-4xl font-bold text-blue-600 mb-6">Cards<span className="text-black text-sm">Coming Soon...</span></h1>
 
         {/* Card Stack Image or SVG */}
         <div className="relative w-full max-w-sm h-auto mb-8 flex justify-center">
@@ -32,26 +32,30 @@ export default function CardPage() {
 
         {/* Action Cards */}
         <div className="space-y-4 w-full max-w-md text-left">
-          <Card className="flex items-center justify-between px-4 py-3 cursor-pointer hover:shadow-md transition">
+          <Card className="flex justify-between px-4 py-3 cursor-pointer hover:shadow-md transition">
             <div className="flex items-start gap-3">
               <div className="w-6 h-6 bg-gray-200 rounded-full mt-1" />
               <div>
                 <p className="text-sm font-semibold text-gray-800">Request A Card</p>
-                <p className="text-xs text-gray-500">We will send it to you wherever you are</p>
+                <p className="text-xs text-gray-500">Get a virtual card for your local and international payments</p>
               </div>
+             <ArrowRight className="h-4 w-4 text-gray-400" />
             </div>
-            <ArrowRight className="h-4 w-4 text-gray-400" />
           </Card>
 
-          <Card className="flex items-center justify-between px-4 py-3 cursor-pointer hover:shadow-md transition">
-            <div className="flex items-start gap-3">
-              <div className="w-6 h-6 bg-gray-200 rounded-full mt-1" />
+          <Card className="flex justify-between px-4 py-3 cursor-pointer hover:shadow-md transition">
+            <div className="flex items-start justify-between gap-3">
+              <div className='flex gap-x-3'>
+                <div className="w-6 h-6 bg-gray-200 rounded-full mt-1" />
+                <div>
+                  <p className="text-sm font-semibold text-gray-800">Card FAQs</p>
+                  <p className="text-xs text-gray-500">Learn more about Gbese card</p>
+                </div>
+              </div> 
               <div>
-                <p className="text-sm font-semibold text-gray-800">Card FAQs</p>
-                <p className="text-xs text-gray-500">Learn more about Gbese card</p>
+                <ArrowRight className="h-4 w-4 text-gray-400" />
               </div>
             </div>
-            <ArrowRight className="h-4 w-4 text-gray-400" />
           </Card>
         </div>
       </main>
