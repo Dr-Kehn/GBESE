8b8648bd92ec1f7283dce6ee7efc1d5a4bc33119
Kehn <ezeokpokc@yahoo.com>
2025-05-05 16:25:08 +0100

    navbar changes

--- src/app/w2/dashboard/borrow/loan/page.tsx
+++ src/app/w2/dashboard/borrow/loan/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { useRouter } from "next/navigation";
-import TopNavbar from "@/components/layout/TopNavbar";
 import BalanceCard from "@/components/dashboard/BalanceCard";
 
 export default function LoanForm() {
@@ -9,8 +8,6 @@ export default function LoanForm() {
 
   return (
     <div className="min-h-screen bg-white">
-      {/* Navbar */}
-      <TopNavbar />
 
       <div className="max-w-4xl mx-auto p-4 sm:p-6">
         {/* Balance Card */}
