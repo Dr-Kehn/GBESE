5a7bce0d9ec59af2912e9fc39117bd6840455716
Tochy112 <ukwuomatochi112@gmail.com>
2025-05-06 20:20:44 +0100

    updated the web3 dashboard

--- src/components/w3/Cards/CryptoCard.tsx
+++ src/components/w3/Cards/CryptoCard.tsx
@@ -1,5 +1,6 @@
 "use client";
 import { useState } from "react";
+import Image from "next/image";
 
 // Main component that renders all crypto cards
 export const CryptoCards = () => {
@@ -10,11 +11,16 @@ export const CryptoCards = () => {
           <div className="bg-[#1a1a1a] rounded-lg p-6 flex justify-between">
             <div className="flex items-center gap-2">
               <div className="w-8 h-8 bg-blue-500 rounded-full flex items-center justify-center">
-                <span className="text-xs font-bold">BTC</span>
+                <Image
+                  src={"/gbese-icon.svg"}
+                  alt="gbese token"
+                  width={50}
+                  height={50}
+                />
               </div>
               <div>
-                <div className="text-white">Bitcoin</div>
-                <div className="text-gray-400 text-xs">0.0045 BTC</div>
+                <div className="text-white">Gbese</div>
+                <div className="text-gray-400 text-xs">5 GB</div>
               </div>
             </div>
             <div className="text-right">
@@ -25,11 +31,27 @@ export const CryptoCards = () => {
 
           <div className="bg-[#1a1a1a] rounded-lg p-6 flex justify-between">
             <div className="flex items-center gap-2">
-              <div className="w-8 h-8 bg-green-500 rounded-full flex items-center justify-center">
-                <span className="text-xs font-bold">ETH</span>
+              <div className="w-8 h-8 rounded-full flex items-center justify-center">
+                <Image src={"/USDC.webp"} alt="usdc" width={60} height={60} />
               </div>
               <div>
-                <div className="text-white">Ethereum</div>
+                <div className="text-white">Usdc</div>
+                <div className="text-gray-400 text-xs">50 USDC</div>
+              </div>
+            </div>
+            <div className="text-right">
+              <div className="text-white">$50</div>
+              <div className="text-green-500 text-xs">+2.3%</div>
+            </div>
+          </div>
+
+          <div className="bg-[#1a1a1a] rounded-lg p-6 flex justify-between">
+            <div className="flex items-center gap-2">
+              <div className="w-8 h-8 rounded-full flex items-center justify-center">
+                <Image src={"/eth-logo.png"} alt="usdc" width={60} height={60} />
+              </div>
+              <div>
+                <div className="text-white">Eth</div>
                 <div className="text-gray-400 text-xs">0.23 ETH</div>
               </div>
             </div>
