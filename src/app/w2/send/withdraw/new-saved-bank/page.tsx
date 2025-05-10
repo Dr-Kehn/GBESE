d498468fdaf0ac65fe93509bf5e4ada313f65290
K-Plaw <praislaw@gmail.com>
2025-05-10 14:56:54 +0100

    Work tree clean?

---
+++ src/app/w2/send/withdraw/new-saved-bank/page.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import React from 'react';
+
+const NewSavedBank: React.FC = () => {
+  return (
+    <div className="p-6">
+      <h1 className="text-2xl font-semibold">Add a New Bank</h1>
+      <p className="text-gray-500">This page will allow users to save a new bank account.</p>
+    </div>
+  );
+};
+
+export default NewSavedBank; // ✅ You must have this line
